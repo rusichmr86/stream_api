02da660355241b5609362d2c468401f9cf872dd0
r.gorbunov <r.gorbunov@tinkoff.ru>
2018-12-23 16:35:14 +0300

    Revert "task3,4,6,7,8 v1.0"
    This reverts commit 9ac38f3

--- src/main/java/Consts.java
+++
@@ -1,6 +0,0 @@
-class Consts {
-    static final String PATHS_TASK_3 = "src\\main\\resources\\task3.txt";
-    static final String PATHS_TASK_6 = "src\\main\\resources\\task6.txt";
-    static final int NUMBER_OF_THREADS = 20;
-
-}
--- src/main/java/Task_8.java
+++
@@ -1,7 +0,0 @@
-class Task_8 {
-    static void task_8() {
-        Methods.taskStart(8);
-        Methods.fizzBuzz();
-        Methods.taskEnd(8);
-    }
-}
